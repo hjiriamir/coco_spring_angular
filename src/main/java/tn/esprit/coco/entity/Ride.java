07af72530cc8c958c8af9930aeb9731b33c6646f
hjiriamir <amir.hjiri@esprit.tn>
2024-04-30 11:40:36 +0200

    integrationspring

--- src/main/java/tn/esprit/coco/entity/Ride.java
+++ src/main/java/tn/esprit/coco/entity/Ride.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.Date;
 import java.util.List;
 
 @Getter
@@ -26,6 +27,18 @@ public class Ride {
     private boolean Smoking_Vehicle;
     private String preference;
     private boolean AvoidTolls;
+    private String TempsEstime;
+    private String Distance;
+    //image
+    private Date DateAddedImage;
+    private String timeImage;
+    private String FormatImage;
+    private String PathImage;
+    private String DescriptionImage;
+    //car
+    private String ModelCar;
+    private Boolean ComfortCar;
+    private String carburant;
     @JsonIgnore
     @OneToMany(mappedBy = "ride")
     private List<Car> cars;
